ad33efc8250638b21b97f4108d0f28e77f518eba
mnlk2 <mottier.ludovic@free.fr>
2011-05-23 05:11:22 +0200

    add NLUpdater

--- src/fr/nameless/NLParts/Main.java
+++ src/fr/nameless/NLParts/Main.java
@@ -1,10 +1,12 @@
 package fr.nameless.NLParts;
 
 import android.app.TabActivity;
+import android.content.ComponentName;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.widget.TabHost;
+
 
 public class Main extends TabActivity { 
 	public void onCreate(Bundle savedInstanceState) {
@@ -18,30 +20,31 @@ public class Main extends TabActivity {
 
 	    // System
 	    intent = new Intent().setClass(this, System.class);
-	    spec = tabHost.newTabSpec("System").setIndicator("System",
+	    spec = tabHost.newTabSpec("System").setIndicator(getString(R.string.system),
 	                      res.getDrawable(R.drawable.tab_icon))
 	                  .setContent(intent);
 	    tabHost.addTab(spec);
 
 	    // Lockscreen
 	    intent = new Intent().setClass(this, Lockscreen.class);
-	    spec = tabHost.newTabSpec("Lockscreen").setIndicator("Lockscreen",
+	    spec = tabHost.newTabSpec("Lockscreen").setIndicator(getString(R.string.lockscreen),
 	                      res.getDrawable(R.drawable.tab_icon))
 	                  .setContent(intent);
 	    tabHost.addTab(spec);
 
 	    // Divers
 	    intent = new Intent().setClass(this, Other.class);
-	    spec = tabHost.newTabSpec("Autre").setIndicator("Autre",
+	    spec = tabHost.newTabSpec("Autre").setIndicator(getString(R.string.divers),
 	                      res.getDrawable(R.drawable.tab_icon))
 	                  .setContent(intent);
 	    tabHost.addTab(spec);
 
 	    // Updates
-	    intent = new Intent().setClass(this, Updater.class);
-	    spec = tabHost.newTabSpec("Updates").setIndicator("Updates",
+	    intent = new Intent().setClassName("com.jojolejobar.nlupdater", "com.jojolejobar.nlupdater.NLUpdater");
+	    spec = tabHost.newTabSpec("Updates").setIndicator(getString(R.string.update),
 	                      res.getDrawable(R.drawable.tab_icon))
 	                  .setContent(intent);
+
 	    tabHost.addTab(spec);
 	    
 	    tabHost.setCurrentTab(0);
